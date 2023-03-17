32d6c4d330177fa5823715d181011ee064013db8
Binh Cao <binh17122001@gmail.com>
2023-03-17 22:05:56 +0700

    test

--- src/screens/NumberPlateScanner/NumberPlateScanner.tsx
+++ src/screens/NumberPlateScanner/NumberPlateScanner.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const NumberPlateScanner = () => {
     return (
         <View style={styles.container}>
-            <Text>NumberPlateScanner</Text>
+        <Text>NumberPlateScanner</Text>
         </View>
     );
 };
